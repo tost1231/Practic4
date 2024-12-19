42d03951f93bef27730ce4337cb38e0d17a01935
tost1231 <freefaer1212@gmail.com>
2024-12-20 02:49:50 +0300

    Signed-off-by: tost1231 <freefaer1212@gmail.com>

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Footer.css';
+
+const Footer = ({ title }) => {
+  return (
+    <footer className="footer">
+      {title}
+    </footer>
+  );
+};
+
+export default Footer;
